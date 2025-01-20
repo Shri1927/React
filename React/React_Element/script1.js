47f7b06671e63c2ba019735e1e556da09f5e006c
Shrishail  Bidave <167215447+Shri1927@users.noreply.github.com>
2025-01-20 22:15:38 +0530

    react_element

--- React/React_Element/script1.js
+++ React/React_Element/script1.js
@@ -1,26 +1,47 @@
-// const h2 = React.createElement('h2', { className: 'heading' }, 'Hello World');
-// ReactDOM.render(h2, document.getElementById('root'));
 
 
-// const h2 = {
-//     $$typeof: Symbol.for("react.element"),
-//     type: "h2",
-//     key: null,
-//     ref: null,
-//     props: {
-//         className: "heading",
-//         children: "Hello World"
-//     },
-// }
-ReactDOM.createRoot(document.getElementById('root')).render(
-    {
-        $$typeof: Symbol.for("react.element"),
-        type: "h2",
-        key: null,
-        ref: null,
-        props: {
-            className: "heading",
-            children: "Hello World"
-        },
-    }
+const container = React.createElement('div', { className: 'container' },
+    React.createElement('section',{}, 
+        React.createElement('p', {}, 'jhdbvhabibjbhbjhhbhjvh')
+    )
 );
+
+// ReactDOM.createRoot(document.querySelector('#root'));
+// ReactDOM.render(container);
+
+
+// // const h2 = React.createElement('h2', { className: 'heading' }, 'Hello World');
+ReactDOM.render(container, document.getElementById('root'));
+
+
+// // const h2 = {
+// //     $$typeof: Symbol.for("react.element"),
+// //     type: "h2",
+// //     key: null,
+// //     ref: null,
+// //     props: {
+// //         className: "heading",
+// //         children: "Hello World"
+// //     },
+// // }
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     {
+//         $$typeof: Symbol.for("react.element"),
+//         type: "h2",
+//         key: null,
+//         ref: null,
+//         props: {
+//             children: [
+//                 {
+//                     $$typeof: Symbol.for("react.element"),
+//                     type: "p",
+//                     key: null,
+//                     ref: null,
+//                     props: {
+//                         children: "Hello World"
+//                     },
+//                 }
+//             ]
+//         },
+//     }
+// );
